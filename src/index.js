068af1602c9570d3e9c41fbcdff94b7e252259c5
Your Name <digneli14@gmail.com>
2020-07-17 19:04:22 +0200

    agregando archivo comienzo de la app... archivo principal en source y configurando now.json, webpack y el package.json

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import Context from './Context'
+
+import { App } from './App'
+
+ReactDOM.render(
+  <Context.Provider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Context.Provider>,
+  document.getElementById('app'))
